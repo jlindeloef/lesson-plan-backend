6bec0c763ca3268ad91f0b6aef6661c82e11b76e
Johan Lindelöf <j_lindeloef@hotmail.com>
2023-12-22 20:08:10 +0000

    Add review count in post view.

--- lesson_plan_backend/urls.py
+++ lesson_plan_backend/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
     path('', include('comments.urls')),
     path('', include('likes.urls')),
     path('', include('followers.urls')),
-    path('',include('category.urls')),
+    path('category/',include('category.urls')),
     path('', include('reviews.urls')),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,4 @@
-from django.db.models import Count
+from django.db.models import Count, Avg
 from rest_framework import generics, permissions, filters
 from lesson_plan_backend.permissions import IsOwnerOrReadOnly
 from .models import Post
@@ -14,7 +14,9 @@ class PostList(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Post.objects.annotate(
         likes_count=Count('likes', distinct=True),
-        comments_count=Count('comment', distinct=True)
+        comments_count=Count('comment', distinct=True),
+        reviews_count=Count('reviews', distinct=True),
+        average_rating=Avg('reviews__rating')
     ).order_by('-created_at')
     filter_backends = [
         filters.OrderingFilter,
@@ -42,5 +44,7 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [IsOwnerOrReadOnly]
     queryset = Post.objects.annotate(
         likes_count=Count('likes', distinct=True),
-        comments_count=Count('comment', distinct=True)
+        comments_count=Count('comment', distinct=True),
+        reviews_count=Count('reviews', distinct=True),
+        average_rating=Avg('reviews__rating')
     ).order_by('-created_at')
