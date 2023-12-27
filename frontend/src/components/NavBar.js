6959e08088aff8f79a29d0e7113cc70805ec3d97
Johan Lindelöf <j_lindeloef@hotmail.com>
2023-12-27 17:21:40 +0000

    Add logo, favicon and links in navbar.

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,17 +1,18 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import logo from "../assets/logo.png";
 
 const NavBar = () => {
   return (
-    <Navbar expand="md" fixed="top">
+    <Navbar bg="light" expand="md" fixed="top">
       <Container>
         <Navbar.Brand>
-          <img src={logo} alt="logo" height="45" />
+          <img src={logo} alt="logo" height="50" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto text-left">
+          <Nav className="mr-auto text-left">
             <Nav.Link>
               <i className="fas fa-home"></i>Home
             </Nav.Link>
@@ -21,6 +22,17 @@ const NavBar = () => {
             <Nav.Link>
               <i className="fas fa-user-plus"></i>Sign up
             </Nav.Link>
+            <NavDropdown title="Categories" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
           </Nav>
         </Navbar.Collapse>
       </Container>
