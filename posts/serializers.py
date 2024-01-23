454103f5cdaf386a5389166d1420234d2d8c0088
Johan Lindelöf <j_lindeloef@hotmail.com>
2024-01-23 19:10:49 +0000

    Took away imagefilter from post model.

--- posts/serializers.py
+++ posts/serializers.py
@@ -45,7 +45,7 @@ class PostSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'created_at', 'updated_at',
-            'title', 'content', 'image', 'image_filter',
+            'title', 'content', 'image',
             'like_id', 'likes_count', 'comments_count',
             'reviews_count', 'average_rating', 
         ]
